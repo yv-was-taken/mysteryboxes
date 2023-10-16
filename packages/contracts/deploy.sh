d0c0df43b4e3c8cc2bf62efdcf68c2e27a0a3f43
yv-was-taken <yvmail@proton.me>
2023-10-16 01:08:17 -0500

    modify deploy script to prefer forge scripting

--- packages/contracts/deploy.sh
+++ packages/contracts/deploy.sh
@@ -8,13 +8,13 @@ if [ -z "$CHAIN_NAME" ]; then
   exit 1
 fi
 
-CONTRACT_NAME="ExampleNFT"
+CONTRACT_NAME=$CONTRACT_NAME
 
 DEPLOY_OUTPUT="deploys/$CHAIN_NAME/$CONTRACT_NAME.json"
 mkdir -p $(dirname $DEPLOY_OUTPUT)
 
 if [ ! -f $DEPLOY_OUTPUT ] || [ ! -s $DEPLOY_OUTPUT ]; then
-  forge create $CONTRACT_NAME --json --rpc-url=$RPC_URL --private-key=$DEPLOYER_PRIVATE_KEY | jq . > $DEPLOY_OUTPUT
+  forge script $CONTRACT_NAME --json --rpc-url=$RPC_URL --private-key=$DEPLOYER_PRIVATE_KEY | jq . > $DEPLOY_OUTPUT
 fi
 
 CONTRACT_ADDRESS=$(cat $DEPLOY_OUTPUT | jq -r ".deployedTo")
@@ -25,4 +25,3 @@ fi
 
 echo "Using $CHAIN_NAME contract address: $CONTRACT_ADDRESS"
 
-# cast send --rpc-url=$RPC_URL $CONTRACT_ADDRESS "setBaseTokenURI(string)" "ipfs://somehashgoeshere" --private-key=$DEPLOYER_PRIVATE_KEY
